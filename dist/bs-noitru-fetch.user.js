ea123249e1b50c7841db6eec7fafb9f77b246d31
Tran Quoc Hoai <bsquochoainew@gmail.com>
2025-08-01 12:20:12 +0700

    uodate

--- dist/bs-noitru-fetch.user.js
+++ dist/bs-noitru-fetch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BS Nội trú - Helper (TA Hospital) - By drquochoai, BS.CKI Trần Quốc Hoài
 // @namespace    http://tampermonkey.net/
-// @version      1.4.13
+// @version      1.5.1
 // @description  Hỗ trợ dữ liệu bệnh nhân từ bs-noitru.tahospital.vn.
 // @author       BS.CKI Trần Quốc Hoài, tahospital.vn
 // @match        https://bs-noitru.tahospital.vn/*
